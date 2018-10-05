5ea3cafba6543c5246318138b259983ffcecbb98
OTHSCS012 <OTHSCS012@KATYISD.ORG>
2018-10-05 13:39:13 -0500

    End of class 10/5/2018: Fixed the SoftPWM_GPIO class to map a RANGE not a duty cycle (which is conceptually wrong). Added new TODOs.

--- drone-code/drone-code/SoftPWM_GPIO.cpp
+++ drone-code/drone-code/SoftPWM_GPIO.cpp
@@ -2,9 +2,9 @@
 #include <softPwm.h>
 #include "SoftPWM_GPIO.h"
 
-SoftPWM_GPIO::SoftPWM_GPIO(int bcm_pin, int _duty_cycle = 100): GPIO(bcm_pin, OUTPUT, PUD_OFF){
-	softPwmCreate(bcm_pin, 0, _duty_cycle);
-	duty_cycle = _duty_cycle;
+SoftPWM_GPIO::SoftPWM_GPIO(int bcm_pin, int _range = 100): GPIO(bcm_pin, OUTPUT, PUD_OFF){
+	softPwmCreate(bcm_pin, 0, _range);
+	range = _range;
 	value = 0;
 }
 
@@ -14,7 +14,7 @@ SoftPWM_GPIO::~SoftPWM_GPIO(){
 }
 
 int SoftPWM_GPIO::get_duty() {
-	return duty_cycle;
+	return value / range;
 }
 
 void SoftPWM_GPIO::change_value(int change) {
--- drone-code/drone-code/SoftPWM_GPIO.h
+++ drone-code/drone-code/SoftPWM_GPIO.h
@@ -5,16 +5,13 @@
 class SoftPWM_GPIO : public GPIO {
 
 	private:
-		int duty_cycle;
+		int range;
 
 	public:
-		SoftPWM_GPIO(int bcm_pin, int duty_cycle = 100);
+		SoftPWM_GPIO(int bcm_pin, int range = 100);
 		~SoftPWM_GPIO();
 
-		//void change_duty(int change);
 		int  get_duty();
-		//void set_duty(int _duty_cycle);
-
 		void change_value(int change);
 		void set_value(int _value);
 };
