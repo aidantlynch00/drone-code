1e9af806e13344e7384d6e6239e34e2c7c5240f2
OTHSCS012 <OTHSCS012@KATYISD.ORG>
2018-11-08 13:21:02 -0600

    Merging PID into master

--- drone-code/drone-code/Main.cpp
+++ drone-code/drone-code/Main.cpp
@@ -1,6 +1,7 @@
 #include <wiringPi.h>
 #include "GPIO.h"
 #include "SoftPWM_GPIO.h"
+#include <iostream>
 
 // LED Pin - wiringPi pin 0 is BCM_GPIO 17.
 // we have to use BCM numbering when initializing with wiringPiSetupSys
@@ -8,10 +9,11 @@
 // update the Property Pages - Build Events - Remote Post-Build Event command 
 // which uses gpio export for setup for wiringPiSetupSys
 
+using namespace std;
 int main(void)
 {
 	wiringPiSetupGpio();
-
+	cout << "Hello World!" << endl;
 
 	return 0;
 }
