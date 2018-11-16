41af4f2aa134dd7318d62ab6543ef7c352d365e1
OTHSCS011 <OTHSCS011@KATYISD.ORG>
2018-11-16 13:09:02 -0600

    I2c Bus stuff

--- drone-code/drone-code/BerryIMU.cpp
+++ drone-code/drone-code/BerryIMU.cpp
@@ -1,5 +1,7 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
-#include <studio.h>
+#include <stdio.h>
 #include <fcntl.h>
-#include "i2c-dev.h"
+#include "linux/i2c-dev.h"
+#include "LSM9DS0.h"
+#include "LSM9DS1.h"
