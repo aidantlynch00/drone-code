292382b8c9adc69a47ce4414bb03839b5ce7ccee
OTHSCS011 <OTHSCS011@KATYISD.ORG>
2018-11-26 12:59:34 -0600

    New stuff

--- drone-code/drone-code/BerryIMU.cpp
+++ drone-code/drone-code/BerryIMU.cpp
@@ -1,7 +1,11 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
+#include <stdint.h>
 #include <stdio.h>
-#include <fcntl.h>
 #include "linux/i2c-dev.h"
 #include "LSM9DS0.h"
 #include "LSM9DS1.h"
+
+int file; 
+int LSM9DS0 = 0;
+int LSM9DS1 = 0;
