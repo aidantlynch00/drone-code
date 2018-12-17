e0a1c72b940aaeddc47bfe491f4c7e501fd369c9
OTHSCS011 <OTHSCS011@KATYISD.ORG>
2018-12-17 13:12:41 -0600

    Even the mosterest Morererest Newerest Mathyer Less speedier correct Stuff

--- drone-code/drone-code/BerryIMU.cpp
+++ drone-code/drone-code/BerryIMU.cpp
@@ -3,12 +3,10 @@
 #include <stdint.h>
 #include <stdio.h>
 #include "linux/i2c-dev.h"
-#include "LSM9DS0.h"
 #include "LSM9DS1.h"
 #include "BerryIMU.h"
 
 int file; 
-int LSM9DS0 = 0;
 int LSM9DS1 = 0;
 
 BerryIMU::BerryIMU(int accelReg, int gyroReg){
