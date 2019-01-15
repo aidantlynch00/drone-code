9b03cb8ba47a322d265953532f2b6d566a9386c5
OTHSCS011 <OTHSCS011@KATYISD.ORG>
2019-01-15 13:41:50 -0600

    Even the mosterest Morererest Newerest Mathyer Less speedier correcterly Stuff this time magnetometer

--- drone-code/drone-code/BerryIMU.h
+++ drone-code/drone-code/BerryIMU.h
@@ -13,10 +13,12 @@ public:
 	~BerryIMU();
 	double* readAccel(); //   m/sec^2 in x, y, z
 	double* readGyro(); // angular acceleration
+	double* readMag();
 	void enableIMU();
 	void detectIMU();
 	void writeAccelReg(uint8_t reg, uint8_t value);
 	void writeGyroReg(uint8_t reg, uint8_t value);
+	void writeMagReg(uint8_t reg, uint8_t value);
 	void selectDevice(int file, int addr);
 	void  readBlock(uint8_t command, uint8_t size, uint8_t *data);
 };
